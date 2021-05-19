53afa6342cea4cdd67180e4d7b4c0867d3b2b373
Vladislav Tsarenko <vadiktsarenko@mail.ru>
2021-05-19 21:14:20 +0300

    Change models in authorization

--- app/authorization/models.py
+++ app/authorization/models.py
@@ -4,23 +4,21 @@ from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
 
 
-class Account(AbstractUser):
-    username = None
-    email = models.EmailField(_('email address'), unique=True)
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+class User(AbstractUser):
+    role = models.SmallIntegerField()
+    # department = models.ForeignKey(Department, on_delete = models.RESTRICT)
+    department = models.IntegerField()
+    block = models.BooleanField(default=False)
 
     def __str__(self):
         return self.email
 
 
-class User(models.Model):
-    account = models.OneToOneField(Account, on_delete=models.CASCADE, primary_key=True)
-    name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
-    role = models.SmallIntegerField()
-    # department = models.ForeignKey(Department, on_delete = models.RESTRICT)
-    department = models.IntegerField()
-    last_authorization = models.DateField()
-    block = models.BooleanField(default=False)
+
+
+
+
+
+
+
 
