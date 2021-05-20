3d45cda5b941a45ab1f71176d94019f01fc97302
Vladislav Tsarenko <vadiktsarenko@mail.ru>
2021-05-20 22:56:47 +0300

    create view for task

--- app/user/views.py
+++ app/user/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from django.views.decorators.http import require_POST
 
 def index(request):
     pass
+
+
+@require_POST
+def create_task(request):
+    """Сохраняет отправленный с формы task в БД"""
