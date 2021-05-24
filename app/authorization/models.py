4577069afba0e6474b155b661f43aa94aec83ff8
Vladislav Tsarenko <vadiktsarenko@mail.ru>
2021-05-24 11:41:26 +0300

    Add Department Foreign Key to User model

--- app/authorization/models.py
+++ app/authorization/models.py
@@ -2,12 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 from django.utils.translation import ugettext_lazy as _
-
+from user.models import Department
 
 class User(AbstractUser):
     role = models.SmallIntegerField(default=1)
-    # department = models.ForeignKey(Department, on_delete = models.RESTRICT)
-    department = models.IntegerField(null=True)
+    department = models.ForeignKey(Department, on_delete=models.RESTRICT)
     post = models.CharField(max_length=200)
     block = models.BooleanField(default=False)
 
