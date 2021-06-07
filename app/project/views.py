f9f48b261f5f0a1b3b17df6db58d2e13d0869eaf
Vladislav Tsarenko <vadiktsarenko@mail.ru>
2021-06-07 23:51:33 +0300

    add project application tests

--- app/project/views.py
+++ app/project/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.db.models import RestrictedError
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.http import Http404
 
 from user.models import Company, Project
 from main.views import decorator_adds_user_information_log
@@ -14,8 +15,11 @@ from project.forms import ProjectForm
 @decorator_check_admin
 def project_list(request, company_id):
     """Просмотр списка проектов компании"""
-    projects = Project.objects.filter(company_id=company_id)
-    company = Company.objects.get(id=company_id)
+    try:
+        projects = Project.objects.filter(company_id=company_id)
+        company = Company.objects.get(id=company_id)
+    except Company.DoesNotExist:
+        raise Http404
     return render(request, "administrator/project/index.html", context={"projects": projects, "company": company})
 
 
@@ -25,9 +29,17 @@ def project_list(request, company_id):
 def edit_project(request, company_id, id=None):
     """Добавление и изменение проекта"""
     if id:
-        project = Project.objects.get(id=id)
+        try:
+            project = Project.objects.get(id=id)
+        except Project.DoesNotExist:
+            raise Http404
     else:
         project = None
+
+    try:
+        company = Company.objects.get(id=company_id)
+    except Company.DoesNotExist:
+        raise Http404
 
     if request.method == "POST":
         form = ProjectForm(request.POST)
@@ -43,7 +55,7 @@ def edit_project(request, company_id, id=None):
     if project:
         form = ProjectForm(instance=project)
     else:
-        form = ProjectForm(initial={'company': Company.objects.get(id=company_id)})
+        form = ProjectForm(initial={'company': company})
 
     return render(request, "base_form.html", context={
         "form": form,
@@ -57,7 +69,11 @@ def edit_project(request, company_id, id=None):
 @decorator_check_admin
 def delete_project(request, company_id, id):
     """удаление проекта"""
-    project = Project.objects.get(id=id)
+    try:
+        project = Project.objects.get(id=id)
+        Company.objects.get(id=company_id)
+    except (Project.DoesNotExist, Company.DoesNotExist):
+        raise Http404
     if project:
         try:
             project.delete()
