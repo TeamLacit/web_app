005142da32bcd0bd5ae3daf3a744b8de689b459d
Vladislav Tsarenko <vadiktsarenko@mail.ru>
2021-06-07 22:35:33 +0300

    add department application tests

--- app/department/views.py
+++ app/department/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.db.models import RestrictedError
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.http import Http404
 
 from user.models import Company, Department
 from main.views import decorator_adds_user_information_log
@@ -14,8 +15,11 @@ from department.forms import DepartmentForm
 @decorator_check_admin
 def department_list(request, company_id):
     """Просмотр списка отделов компании"""
-    departments = Department.objects.filter(company_id=company_id)
-    company = Company.objects.get(id=company_id)
+    try:
+        departments = Department.objects.filter(company_id=company_id)
+        company = Company.objects.get(id=company_id)
+    except Company.DoesNotExist:
+        raise Http404
     return render(request, "administrator/department/index.html", context={"departments": departments, "company": company})
 
 
@@ -25,9 +29,17 @@ def department_list(request, company_id):
 def edit_department(request, company_id, id=None):
     """Добавление и изменение отдела"""
     if id:
-        department = Department.objects.get(id=id)
+        try:
+            department = Department.objects.get(id=id)
+        except Department.DoesNotExist:
+            raise Http404
     else:
         department = None
+
+    try:
+        company = Company.objects.get(id=company_id)
+    except Company.DoesNotExist:
+        raise Http404
 
     if request.method == "POST":
         form = DepartmentForm(request.POST)
@@ -35,7 +47,8 @@ def edit_department(request, company_id, id=None):
             if not department:
                 department = Department()
             department.name = form.cleaned_data['name']
-            department.company = Company.objects.get(id=company_id)
+            department.company = company
+            department.save()
             projects = form.cleaned_data['project']
             for project in projects:
                 department.project.add(project)
@@ -57,7 +70,11 @@ def edit_department(request, company_id, id=None):
 @decorator_check_admin
 def delete_department(request, company_id, id):
     """удаление отдела"""
-    department = Department.objects.get(id=id)
+    try:
+        department = Department.objects.get(id=id)
+        Company.objects.get(id=company_id)
+    except (Department.DoesNotExist, Company.DoesNotExist):
+        raise Http404
     if department:
         try:
             department.delete()
